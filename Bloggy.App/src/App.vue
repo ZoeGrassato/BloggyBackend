582bdff455fbe44bcc9ec8bd5f26bb4acd09679a
Zoe Lucinda <zoelucindagrassato@gmail.com>
2020-09-14 16:04:50 +0200

    initial for frontend

--- Bloggy.App/src/App.vue
+++ Bloggy.App/src/App.vue
@@ -1,32 +1,47 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div id="app">
+        <div id="nav">
+            <div class="row w-100">
+                <div class="col-1">
+                    <img class="shapes-sizing" src="./assets/navbar-shapes.png">
+                    <router-link to="/">Bloggy</router-link>
+                </div>
+                <div class="col-2">Hello</div>
+            </div>
+            <router-link to="/">Home</router-link> |
+            <router-link to="/about">About</router-link>
+        </div>
+        <router-view />
     </div>
-    <router-view/>
-  </div>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+    #app {
+        font-family: Avenir, Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+    }
 
-#nav {
-  padding: 30px;
-}
+    #nav {
+        height: 126px;
+        padding: 0px;
+        background: rgba(0, 0, 0, 0.81);
+    }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+        #nav a {
+            font-weight: bold;
+            color: #2c3e50;
+        }
 
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+            #nav a.router-link-exact-active {
+                color: #42b983;
+            }
+
+    .shapes-sizing {
+        height: 126px;
+        transform: rotateX(180deg) rotateY(180deg);
+        margin-left: -14px;
+    }
 </style>
--- Bloggy.App/src/main.js
+++ Bloggy.App/src/main.js
@@ -2,11 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import bootstrap from 'bootstrap'
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
+  bootstrap,
   render: h => h(App)
 }).$mount('#app')
